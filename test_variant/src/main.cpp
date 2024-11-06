89387e4f2fe14420a7a4bf35638e1a149acd0dba
Richard Pöttler <richard.poettler@flowengineering.at>
2024-11-06 13:39:23 +0100

    Test variant with optional

--- test_variant/src/main.cpp
+++ test_variant/src/main.cpp
@@ -1,3 +1,4 @@
+#include <optional>
 #include <print>
 #include <string>
 #include <variant>
@@ -6,20 +7,14 @@ class Material {
    public:
     int id;
     std::string name;
-    std::variant<std::string, int, double> value;
+    std::variant<int, double, std::optional<std::string>> value;
 };
 
 int main() {
     Material m1{1, "testmat", 5};
     std::println("m1: {}, {}", m1.id, m1.name);
-    if (std::holds_alternative<std::string>(m1.value)) {
-        std::println("holds a sting");
-    }
     if (std::holds_alternative<int>(m1.value)) {
         std::println("holds an int");
-    }
-    if (std::holds_alternative<double>(m1.value)) {
-        std::println("holds a double");
     }
     if (const auto v = std::get_if<int>(&m1.value)) {
         std::println("got int: {}", *v);
@@ -31,5 +26,24 @@ int main() {
     } else {
         std::println("get_if with 0 evaluates to false");
     }
+
+    Material m_string{1, "testmat", "something"};
+    if (const auto ov =
+            std::get_if<std::optional<std::string>>(&m_string.value)) {
+        if (const auto v = *ov) {
+            std::println("m_string: {}", *v);
+        }
+    }
+
+    Material m_null{1, "testmat", std::nullopt};
+    if (const auto ov =
+            std::get_if<std::optional<std::string>>(&m_null.value)) {
+        if (const auto v = *ov) {
+            std::println("m_null: {}", *v);
+        } else {
+            std::println("m_null: get_if evaluates to true but value is null");
+        }
+    }
+
     return 0;
 }
