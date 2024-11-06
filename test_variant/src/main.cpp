0962514db30bc5e7c125572dcbcce3ff824e34b8
Richard Pöttler <richard.poettler@flowengineering.at>
2024-11-06 13:17:18 +0100

    Use std::println

--- test_variant/src/main.cpp
+++ test_variant/src/main.cpp
@@ -11,18 +11,18 @@ class Material {
 
 int main() {
     Material m1{1, "testmat", 5};
-    std::print("m1: {}, {}\n", m1.id, m1.name);
+    std::println("m1: {}, {}", m1.id, m1.name);
     if (std::holds_alternative<std::string>(m1.value)) {
-        std::print("holds a sting\n");
+        std::println("holds a sting");
     }
     if (std::holds_alternative<int>(m1.value)) {
-        std::print("holds an int\n");
+        std::println("holds an int");
     }
     if (std::holds_alternative<double>(m1.value)) {
-        std::print("holds a double\n");
+        std::println("holds a double");
     }
     if (const auto v = std::get_if<int>(&m1.value)) {
-        std::print("got int: {}\n", *v);
+        std::println("got int: {}", *v);
     }
 
     Material m0{1, "zero mat", 0};
